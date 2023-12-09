e35144e6e0ee1fd3428abb235d5f9bb95ef61d04
Aleksey Strizhenko <lokolex310188@gmail.com>
2023-12-09 20:47:42 +0400

    Added Error component and Loading component

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -4,6 +4,7 @@ import Thumbnail from '@/components/Thumbnail';
 import { fetchVideos } from '@/lib/api';
 import { useState } from 'react';
 import useSWR from 'swr';
+import Loading from '../loading';
 
 export default function Home() {
   const [badge, setBadge] = useState('All');
@@ -13,6 +14,10 @@ export default function Home() {
     isLoading,
   } = useSWR(`fetchVideo/${badge}`, () => fetchVideos(badge, 9));
 
+  if (error) {
+    throw new Error('Error fetching video data.');
+  }
+
   return (
     <>
       <div className="px-2 md:pl-[252px] fixed top-16 py-2 left-0 w-screen z-20 dark:bg-black bg-white">
@@ -20,6 +25,10 @@ export default function Home() {
       </div>
 
       <div className="flex flex-wrap ">
+        {isLoading &&
+          Array(9)
+            .fill(null)
+            .map((el, idx) => <Loading key={idx} />)}
         {videoResults?.map((video) => (
           <Thumbnail key={video.id} video={video} />
         ))}
